7397cab92c8eb17ee5237ea960690920f4fd9d9a
Abiria <abiria.dev@gmail.com>
2023-12-15 11:56:35 +0900

    feat: basic implementation

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/robfig/cron/v3"
+)
+
+func main() {
+	flag.Parse()
+
+	crx, cmd := flag.Arg(0), flag.Arg(1)
+	if crx == "" {
+		panic("provide cron expression please")
+	}
+	if cmd == "" {
+		panic("provide command to run please")
+	}
+
+	c := cron.New()
+
+	c.AddFunc(crx, func() {
+		fmt.Println(cmd)
+	})
+
+	c.Start()
+}
