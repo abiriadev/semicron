7650f5ab8ff65d76ed2d795e694505e2052b013d
Abiria <abiria.dev@gmail.com>
2023-12-15 13:49:55 +0900

    feat: support arguments

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	flag.Parse()
 
-	crx, cmd := flag.Arg(0), flag.Arg(1)
+	crx, cmd, argv := flag.Arg(0), flag.Arg(1), flag.Args()[2:]
 	if crx == "" {
 		panic("provide cron expression please")
 	}
@@ -28,7 +28,7 @@ func main() {
 	)
 
 	_, err := c.AddFunc(crx, func() {
-		cmd := exec.Command(cmd)
+		cmd := exec.Command(cmd, argv...)
 		cmd.Stdout = os.Stdout
 		err := cmd.Run()
 		if err != nil {
