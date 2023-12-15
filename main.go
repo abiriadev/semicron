6128bb10202d33113dad65bd9343d866f0c06a27
Abiria <abiria.dev@gmail.com>
2023-12-15 13:41:44 +0900

    feat: support optional second

--- main.go
+++ main.go
@@ -18,7 +18,13 @@ func main() {
 		panic("provide command to run please")
 	}
 
-	c := cron.New()
+	c := cron.New(
+		cron.WithParser(
+			cron.NewParser(
+				cron.SecondOptional | cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow | cron.Descriptor,
+			),
+		),
+	)
 
 	_, err := c.AddFunc(crx, func() {
 		fmt.Println(cmd)
